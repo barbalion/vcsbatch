cca3649091f932f55db7fc5af52c2b30d9e91306
Alexander Ermakov <barbalion@gmail.com>
2019-12-21 22:29:35 +0300

    minor

--- g/ig.cmd
+++ g/ig.cmd
@@ -11,8 +11,11 @@ if "%1" == "" g h ig
 :loop
 if "%1" == "" goto :EOF
 
-if "%~nx1" == "" %0 "%~1\."
-echo Ignoring "%~nx1"...
-echo %~nx1>>%~dp1.gitignore
+if "%~nx1" == "" (
+  call %0 "%~1\."
+) else (
+  echo Ignoring "%~nx1"...
+  echo %~nx1>>"%~dp1.gitignore"
+)
 shift   
 goto loop
